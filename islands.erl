8f92fb7a7d61d518a7a26a9c7cab0fc453f6ec82
Lewis Heath <email@lewisheath.co.uk>
2024-08-28 22:06:06 +0100

    cleaned up the solution

--- islands.erl
+++ islands.erl
@@ -10,7 +10,7 @@ count_sub_islands(Grid1, Grid2) ->
     % Create a map to track the visited squares on the second grid
     VisitedMap = maps:map(fun(_, _) -> not_visited end, Grid2_Map),
     % Find the islands on the second grid
-    Islands = find_islands(Grid2_Map, VisitedMap),
+    {_, Islands} = find_islands(Grid2_Map, VisitedMap),
     % For each island found, check if EVERY square is a 1 on the first grid
     find_amount_of_islands_on_grid_1(Islands, Grid1_Map).
 
@@ -27,58 +27,36 @@ convert_grid_to_map(Grid) ->
     Map.
 
 find_islands(GridMap, VisitedMap) ->
-    % For each square, if it has not been visited before, perform a depth/breadth first search
-    % to find all coordinates connected that have a value of 1
-    % look right, then down, then left, then up
-    {_, Islands} = find_islands_recursively(GridMap, VisitedMap),
-    % io:format("New Visited Map FINAL: ~p~n", [NewVisitedMap]),
-    % io:format("Found Islands List FINAL: ~p~n", [Islands]),
-    Islands.
-
-find_islands_recursively(GridMap, VisitedMap) ->
-    % loop over the map, performing the search starting from each square on the grid
-    % Accumulator is the {VisitedMap, AccumulatedIslands}
-    maps:fold(fun(Coordinate, Value, {CurrentVisitedMap, AccumulatedIslands}) ->
-        % do stuff
-        % for each square in the map, start a depth first search which will return a new visited map and a list of squares that make an island
-        % or if the square is visited already or a 0, it will return an empty list
-        case find_islands_recursively_(GridMap, CurrentVisitedMap, Coordinate, Value, maps:get(Coordinate, CurrentVisitedMap, visited), []) of
+    maps:fold(fun(Coordinate, _, {CurrentVisitedMap, AccumulatedIslands}) ->
+        % if the square is visited already or a 0, it will return an empty list
+        case find_islands_(GridMap, CurrentVisitedMap, Coordinate, []) of
             {NewVisitedMap, []}              -> {NewVisitedMap, AccumulatedIslands};
             {NewVisitedMap, FoundIslandList} -> {NewVisitedMap, [FoundIslandList | AccumulatedIslands]}
         end
     end, {VisitedMap, []}, GridMap).
 
-% If current square visited, or a 0, abort and return AccumulatedIsland
-find_islands_recursively_(_, Map, Coordinate, 0, _, AccumulatedIsland) ->
-    {maps:put(Coordinate, visited, Map), AccumulatedIsland};
-find_islands_recursively_(_, Map, _, _, visited, AccumulatedIsland) ->
-    {Map, AccumulatedIsland};
-find_islands_recursively_(GridMap, VisitedMap, CurrentCoordinate = {X, Y}, 1, not_visited, AccumulatedIsland) ->
-    % This square is a 1 and has not been visted
-    % add myself to the accumulated island and visited map
-    VisitedMap1 = maps:put(CurrentCoordinate, visited, VisitedMap),
-    AccumulatedIsland1 = [CurrentCoordinate | AccumulatedIsland],
-    % depth search in each direction, update the accumulated island and visited map before choosing another direction
-    % RIGHT
-    RightCoordinate = {X + 1, Y},
-    {VisitedMap2, AccumulatedIsland2} = find_islands_recursively_(GridMap, VisitedMap1, RightCoordinate, maps:get(RightCoordinate, GridMap, 0), maps:get(RightCoordinate, VisitedMap1, visited), AccumulatedIsland1),
-    % DOWN
-    DownCoordinate = {X, Y - 1},
-    {VisitedMap3, AccumulatedIsland3} = find_islands_recursively_(GridMap, VisitedMap2, DownCoordinate, maps:get(DownCoordinate, GridMap, 0), maps:get(DownCoordinate, VisitedMap2, visited), AccumulatedIsland2),
-    % LEFT
-    LeftCoordinate = {X - 1, Y},
-    {VisitedMap4, AccumulatedIsland4} = find_islands_recursively_(GridMap, VisitedMap3, LeftCoordinate, maps:get(LeftCoordinate, GridMap, 0), maps:get(LeftCoordinate, VisitedMap3, visited), AccumulatedIsland3),
-    % UP
-    UpCoordinate = {X, Y + 1},
-    {VisitedMap5, AccumulatedIsland5} = find_islands_recursively_(GridMap, VisitedMap4, UpCoordinate, maps:get(UpCoordinate, GridMap, 0), maps:get(UpCoordinate, VisitedMap4, visited), AccumulatedIsland4),
-    % once that has been done, return the final updated visited map and accumulated island list
-    % io:format("COMPLETED A SEARCH!!~nVISITED MAP: ~p~nACCUMULATED ISLAND: ~p~n", [VisitedMap5, AccumulatedIsland5]),
-    {VisitedMap5, AccumulatedIsland5}.
+find_islands_(GridMap, VisitedMap, CurrentCoordinate = {X, Y}, AccumulatedIsland) ->
+    case {maps:get(CurrentCoordinate, GridMap, 0), maps:get(CurrentCoordinate, VisitedMap, visited)} of
+        {0, _} ->
+            {maps:put(CurrentCoordinate, visited, VisitedMap), AccumulatedIsland}; % square is 0
+        {_, visited} ->
+            {VisitedMap, AccumulatedIsland}; % been visited before
+        _ ->
+            VisitedMap1 = maps:put(CurrentCoordinate, visited, VisitedMap),
+            AccumulatedIsland1 = [CurrentCoordinate | AccumulatedIsland],
+            % depth search in each direction, update the accumulated island and visited map before choosing another direction
+            % RIGHT
+            {VisitedMap2, AccumulatedIsland2} = find_islands_(GridMap, VisitedMap1, {X + 1, Y}, AccumulatedIsland1),
+            % DOWN
+            {VisitedMap3, AccumulatedIsland3} = find_islands_(GridMap, VisitedMap2, {X, Y - 1}, AccumulatedIsland2),
+            % LEFT
+            {VisitedMap4, AccumulatedIsland4} = find_islands_(GridMap, VisitedMap3, {X - 1, Y}, AccumulatedIsland3),
+            % UP
+            find_islands_(GridMap, VisitedMap4, {X, Y + 1}, AccumulatedIsland4)
+    end.
 
 find_amount_of_islands_on_grid_1(Islands, Grid1_Map) ->
-    % for each list of coordinates in the islands list, check if each one is in the grid1 map
     lists:foldl(fun(Island, AmountOfIslandsMatched) ->
-        % if island is on grid1, increment amount of islands matched
         case is_island_on_grid_1(Island, Grid1_Map) of
             true -> AmountOfIslandsMatched + 1;
             _    -> AmountOfIslandsMatched
